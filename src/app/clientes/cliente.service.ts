5f0c8305d87a599c74e315f3ee25c5e780d0aa44
Néstor <nestor.amichetti@gmail.com>
2021-02-03 03:30:39 -0300

    Corrigiendo errores

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,36 +3,35 @@ import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import {of, Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
+  private urlEndPoint:string='http://localhost:8080/api/';
   private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
-  baseURl = environment.baseURL
 
   constructor(private httpClient:HttpClient) {}
 
   public getClientes():Observable<Cliente[]>{
     //return of(CLIENTES);
-    return this.httpClient.get<Cliente[]>(this.baseURl);
+    return this.httpClient.get<Cliente[]>(this.urlEndPoint);
   }
 
   create(cliente:Cliente):Observable<Cliente>{
-    return this.httpClient.post<Cliente>(this.baseURl,cliente,{headers:this.httpHeaders});
+    return this.httpClient.post<Cliente>(this.urlEndPoint,cliente,{headers:this.httpHeaders});
   }
 
   getCliente(id):Observable<Cliente>{
-    return this.httpClient.get<Cliente>(`${this.baseURl}/${id}`);
+    return this.httpClient.get<Cliente>(`${this.urlEndPoint}/${id}`);
   }
 
   update(cliente:Cliente):Observable<Cliente>{
-    return this.httpClient.put<Cliente>(`${this.baseURl}/${cliente.id}`,cliente,{headers:this.httpHeaders});
+    return this.httpClient.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`,cliente,{headers:this.httpHeaders});
   }
 
   delete(id):Observable<Cliente>{
-    return this.httpClient.delete<Cliente>(`${this.baseURl}/${id}`,{headers:this.httpHeaders});
+    return this.httpClient.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers:this.httpHeaders});
   }
 }
